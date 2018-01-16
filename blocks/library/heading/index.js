50f60eb2d0f546e9a4bdbd3d493d2ec5e6dc68af
iseulde <wp@iseulde.com>
2018-01-16 09:56:41 +0100

    Add alignment shortcuts

--- blocks/library/heading/index.js
+++ blocks/library/heading/index.js
@@ -112,17 +112,38 @@ registerBlockType( 'core/heading', {
 							return createBlock( 'core/paragraph', {
 								content,
 							} );
-						} else {
-							return createBlock( 'core/heading', {
-								nodeName: `H${ level }`,
-								content,
-							} );
 						}
+
+						return createBlock( 'core/heading', {
+							nodeName: `H${ level }`,
+							content,
+						} );
 					} );
 				},
 			} ) ),
 		],
 	},
+
+	shortcuts: [
+		{
+			shortcut: 'l',
+			attributes: {
+				align: 'left',
+			},
+		},
+		{
+			shortcut: 'c',
+			attributes: {
+				align: 'center',
+			},
+		},
+		{
+			shortcut: 'r',
+			attributes: {
+				align: 'right',
+			},
+		},
+	],
 
 	merge( attributes, attributesToMerge ) {
 		return {
@@ -141,6 +162,7 @@ registerBlockType( 'core/heading', {
 						'234'.split( '' ).map( ( level ) => ( {
 							icon: 'heading',
 							title: sprintf( __( 'Heading %s' ), level ),
+							shortcut: level,
 							isActive: 'H' + level === nodeName,
 							onClick: () => setAttributes( { nodeName: 'H' + level } ),
 							subscript: level,
@@ -157,6 +179,7 @@ registerBlockType( 'core/heading', {
 							'123456'.split( '' ).map( ( level ) => ( {
 								icon: 'heading',
 								title: sprintf( __( 'Heading %s' ), level ),
+								shortcut: level === '1' ? undefined : level,
 								isActive: 'H' + level === nodeName,
 								onClick: () => setAttributes( { nodeName: 'H' + level } ),
 								subscript: level,
